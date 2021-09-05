08dd9e3ac6296bd567329102e8a4b26d989a35a0
bruno meireles <brunomeireles@brunos-MacBook-Air.local>
2021-09-05 02:28:07 -0300

    first commit

---
+++ src/main/java/br/com/iza/VendasIzaApplication.java
@@ -0,0 +1,13 @@
+package br.com.iza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VendasIzaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VendasIzaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/iza/VendasIzaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.iza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VendasIzaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
